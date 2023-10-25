49da4b4de92e68db8b0d2730f05b89ba408c654a
Shivansh mishra <125989942+Shivansh-243@users.noreply.github.com>
2023-10-25 14:20:52 +0530

    Add files via upload

---
+++ Client/src/Drawing_Tools/PaintBucket.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FormatColorFillIcon from "@mui/icons-material/FormatColorFill";
+
+const PaintBucket = () => {
+  return (
+    <div style={{ display: "grid", justifyContent: "center" }}>
+      <FormatColorFillIcon />
+    </div>
+  );
+};
+
+export default PaintBucket;
